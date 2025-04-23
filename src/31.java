02a47ea7510c07852e2caefaca243da22254aa33
oppytremayne211 <oppytremayne211@gmail.com>
2025-04-23 10:50:05 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Code to execute here
+        System.out.println("This is the main method in Java.");
+        // Additional code can be added here
+    }
+}
