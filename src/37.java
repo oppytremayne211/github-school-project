eaa215c4ed3ae4d63983083c6ccd8d8716fdbdb4
oppytremayne211 <oppytremayne211@gmail.com>
2025-05-17 09:07:12 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,8 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
