d08694d443c7a275c393ef48f843bbd388907d9f
oppytremayne211 <oppytremayne211@gmail.com>
2025-05-20 00:27:15 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,12 @@
+public class SchoolProject {
+    private String name;
+
+    public SchoolProject(String name) {
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Project Name: " + name);
+        // Add more methods as needed for your project
+    }
+}
